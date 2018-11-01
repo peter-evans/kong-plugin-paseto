e836f6f06e16358cde36a6ba550ee351ec4213bc
peter-evans <peter-evans@users.noreply.github.com>
2018-11-01 12:26:06 +0900

    Update plugin setup and db bootstrap

--- .ci/install.sh
+++ .ci/install.sh
@@ -2,17 +2,18 @@
 set -e
 
 export LUA_PATH="$LUA_PATH;$TRAVIS_BUILD_DIR/?.lua;;"
-export KONG_CUSTOM_PLUGINS=$PLUGIN_NAME
+export KONG_TEST_PLUGINS=bundled,$PLUGIN_NAME
+export KONG_PLUGINS=bundled,$PLUGIN_NAME
 
 luarocks install libsodium
 (cd $TRAVIS_BUILD_DIR; luarocks make)
 
 pushd $KONG_DOWNLOAD
-  echo "custom_plugins = $PLUGIN_NAME" >> spec/kong_tests.conf
+  echo "plugins = bundled,$PLUGIN_NAME" >> spec/kong_tests.conf
 
   cp spec/kong_tests.conf spec/kong_tests_cassandra.conf
   sed -i -e 's/database = postgres/database = cassandra/g' spec/kong_tests_cassandra.conf
 
-  bin/kong migrations up -c spec/kong_tests.conf
-  bin/kong migrations up -c spec/kong_tests_cassandra.conf
+  bin/kong migrations bootstrap -c spec/kong_tests.conf
+  bin/kong migrations bootstrap -c spec/kong_tests_cassandra.conf
 popd
